ff27117b64065435e09f579ef00bb6ad60bdb215
Sandeep Edara <edarasp@mail.uc.edu>
2023-03-25 00:29:37 -0400

    Home component

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+    return (
+        <>
+            <Navbar />
+            <div>content here</div>
+        </>
+    )
+}
+
+export default Home

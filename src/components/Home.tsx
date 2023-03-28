68c0721c84af84b0351cd6f75275477c0580ee82
Sandeep Edara <edarasp@mail.uc.edu>
2023-03-27 21:46:58 -0400

    add description

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,9 +6,9 @@ const Home = () => {
     return (
         <>
             <Navbar />
-            <div className='grid grid-cols-2 justify-items-center items-center'>
-                <div>
-                    <div className='flex flex-row'>
+            <div className='grid grid-cols-2 justify-center content-center justify-items-center'>
+                <div className='flex flex-col justify-center justify-items-center items-center p-5'>
+                    <div className='flex flex-row justify-center'>
                         <div className='text-xl'>
                             Hi There
                         </div>
@@ -17,11 +17,11 @@ const Home = () => {
                         </div>
                     </div>
                     <AnimatedText text={`I'am Sandeep Edara`} delay={150} />
-                    {/* <div className='text-5xl'>
-                        I'am Sandeep Edara
-                    </div> */}
+                    <div className='font-thin text-left ml-16 mt-6'>
+                        Welcome to my digital portfolio, a glimpse into my passion for creating innovative software solutions. As a skilled software engineer, I've mastered web and mobile development using cutting-edge technologies. Dive into my projects to uncover my expertise in React Typescript, Node, PostgreSQL, and MongoDB. My commitment to excellence, adaptability, and continuous learning sets me apart. Let's embark on a collaborative journey to drive success in the dynamic world of technology.
+                    </div>
                 </div>
-                <img className="w-60 h-60 m-10 object-cover rounded-full" src='../../public/assets/IMG_5429.jpg' />
+                <img className="w-60 h-60 m-10 object-cover rounded-full opacity-70" src='../../public/assets/IMG_2736.PNG' />
             </div>
         </>
     )
